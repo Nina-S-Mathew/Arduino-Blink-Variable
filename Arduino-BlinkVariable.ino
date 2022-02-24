175292ab22335f6987e1d71e9af21c332b49f8b3
Nina-S-Mathew <99273767+Nina-S-Mathew@users.noreply.github.com>
2022-02-24 11:22:48 -0500

    Update Arduino-BlinkVariable.ino

--- Arduino-BlinkVariable.ino
+++ Arduino-BlinkVariable.ino
@@ -1,5 +1,9 @@
-// C++ code
-//
+/* C++ code 
+ *  LED that turns on one second and off one second. 
+ *  Every time the time off increases by one second.
+ *  Created on Feb 2022
+ *  by Nina Mathew
+*/
 int blinkTime = 1000;
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
